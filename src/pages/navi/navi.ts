a826141a23c5b683818e73b75b43f79325a332d9
yamaji <yamaji@jig.jp>
2022-07-19 17:46:14 +0900

    👍 マップ表示できるように

--- src/pages/navi/navi.ts
+++ src/pages/navi/navi.ts
@@ -10,9 +10,11 @@ export const init = () => {
   }))
 
   const mapElem = document.getElementById('map') as HTMLElement
-  const mapOptions: MapOptions = {
-    center: L.latLng(35.943363, 136.200576),
-    zoom: 12
-  }
-  L.map(mapElem, mapOptions)
+  const map = L.map(mapElem)
+
+  L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
+    attribution: '<a href="https://maps.gsi.go.jp/development/ichiran.html" target="_blank">地理院タイル</a>'
+  }).addTo(map)
+
+  map.setView(L.latLng(35.943363, 136.200576), 20)
 }
