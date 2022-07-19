8e946c269e3714c925cf3902523db4197833a72a
yamaji <yamaji@jig.jp>
2022-07-19 16:24:19 +0900

    👍 ビルド後にSPAとして機能するようにテンプレートの読み込み型を URL() を利用したものに変更

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,8 @@
+/* eslint-disable indent */
 // 参考: https://github.com/melnikaite/alpine-router
 
-import { init as page1Init } from './pages/home/home'
-import { init as page2Init } from './pages/navi/navi'
+import { init as homeInit } from './pages/home/home'
+import { init as naviInit } from './pages/navi/navi'
 import { init as reviewInit } from './pages/review/review'
 
 window.addEventListener('hashchange', onRouteChange)
@@ -16,17 +17,20 @@ async function onRouteChange() {
 async function loadRoute(appElement: HTMLElement) {
   switch (window.location.hash) {
     case '#navi':
-      appElement.innerHTML = await fetch('./pages/navi/navi.html').then(template => template.text())
-      page2Init()
+      appElement.innerHTML = await fetch(new URL('./pages/navi/navi.html', import.meta.url).href)
+                                    .then(template => template.text())
+      naviInit()
       break
     case '#review':
-      appElement.innerHTML = await fetch('./pages/review/review.html').then(template => template.text())
+      appElement.innerHTML = await fetch(new URL('./pages/review/review.html', import.meta.url).href)
+                                    .then(template => template.text())
       reviewInit()
       break
     case '#home':
     default:
-      appElement.innerHTML = await fetch('./pages/home/home.html').then(template => template.text())
-      page1Init()
+      appElement.innerHTML = await fetch(new URL('./pages/home/home.html', import.meta.url).href)
+                                    .then(template => template.text())
+      homeInit()
       break
   }
 }
